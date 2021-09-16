d51ed4e7f9efd1941972a0b2e68f8adac030e2dc
leonardar <leonardar88@gmail.com>
2021-09-16 14:42:28 +0300

    parser v21`

--- parser/my_db.sql
+++ parser/my_db.sql
@@ -1,6 +1,8 @@
 DROP TABLE IF EXISTS recipes;
-DROP TABLE IF EXISTS product_category_entity;
+DROP TABLE IF EXISTS favorites;
+DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS product_entity;
+DROP TABLE IF EXISTS product_category_entity;
 DROP TABLE IF EXISTS dish_entity;
 DROP TABLE IF EXISTS dish_category_entity;
 DROP TABLE IF EXISTS hibernate_sequence;
@@ -36,6 +38,8 @@ CREATE TABLE dish_entity (
     id BIGSERIAL,
     title varchar(255) not null,
 	description TEXT,
+	ingredients_list TEXT,
+	img_path TEXT,
     dish_cat_id int REFERENCES dish_category_entity(id),
 	PRIMARY KEY (id)
 );
@@ -52,6 +56,32 @@ CREATE TABLE recipes
         ON DELETE CASCADE,
     CONSTRAINT dish_id_fk FOREIGN KEY (dish_id)
         REFERENCES dish_entity (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+	PRIMARY KEY(id)
+
+);
+
+CREATE TABLE users
+(
+    id       uuid not null,
+    name     varchar(64) not null,
+    login    varchar(64) not null,
+    password varchar(64) not null,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE favorites
+(
+    id BIGSERIAL,
+    product_id bigint NOT NULL,
+    user_id uuid NOT NULL,
+    CONSTRAINT produck_id_fk FOREIGN KEY (product_id)
+        REFERENCES product_entity (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT user_id_fk FOREIGN KEY (user_id)
+        REFERENCES users (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE,
 	PRIMARY KEY(id)
