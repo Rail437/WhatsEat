2c6cf66ca63cf72f4bfc015932ae8282640ff36e
Aleksei.Polianskii <aleksei.polianskii@mail.ru>
2021-09-28 23:38:57 +0700

    program rebranding

--- DesktopProgram/main.py
+++ DesktopProgram/main.py
@@ -4,7 +4,7 @@ import json
 
 from PyQt5 import QtWidgets
 
-from DesktopProgram.sample.sample import Ui_MainWindow
+from DesktopProgram.sample.sample_new import Ui_WhatsEat
 from DesktopProgram.utils import load_configs, get_address
 
 CONFIG = dict()
@@ -16,13 +16,13 @@ if __name__ == "__main__":
     ADDRESS_SERVER = get_address(CONFIG)
 
     # Response server (TEST)
-    # with open('answer_server_3.json', 'w') as f:
+    # with open('answer_server_2.json', 'w') as f:
     #     answer = session.get('http://185.46.8.32:8080/whatseat/api/v1/dishes?products=')
     #     json.dump(answer.json(), f)
 
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow(session, ADDRESS_SERVER)
+    ui = Ui_WhatsEat(session, ADDRESS_SERVER)
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
